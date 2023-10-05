9d13b3a4e29c032d601b2923a012dd1b6c4d37bd
Andre <andre.huang2010@gmail.com>
2023-10-05 17:51:21 -0300

    Add background color change on mouse hover

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 const container = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for(let j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        row.appendChild(grid);
+function createGrid(size) {
+    for(let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let j = 0; j < size; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            row.appendChild(grid);
+        };
     };
 };
+
+createGrid(16);
+const grid = document.querySelectorAll('.grid');
+
+grid.forEach(gridItem => gridItem.addEventListener("mouseover", () => {
+    gridItem.style.background = 'black';
+}));
