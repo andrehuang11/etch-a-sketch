80b05a8c23b080078a7d2b517808d157b39c9f0f
Andre <andre.huang2010@gmail.com>
2023-10-05 18:37:41 -0300

    Add slider to select grid size

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('#container');
+let slider = document.getElementById("Range");
+let output = document.getElementById("value");
+output.innerHTML = slider.value;
 
 function createGrid(size) {
     for(let i = 0; i < size; i++) {
@@ -13,9 +16,25 @@ function createGrid(size) {
     };
 };
 
-createGrid(16);
-const grid = document.querySelectorAll('.grid');
+function removeGrid() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
+    });
+};
 
+createGrid(slider.value);
+const grid = document.querySelectorAll('.grid');
 grid.forEach(gridItem => gridItem.addEventListener("mouseover", () => {
     gridItem.style.background = 'black';
 }));
+
+slider.addEventListener('input',(e)=>{
+    output.innerHTML = e.target.value;
+    removeGrid();
+    createGrid(e.target.value);
+    const grid = document.querySelectorAll('.grid');
+    grid.forEach(gridItem => gridItem.addEventListener("mouseover", () => {
+        gridItem.style.background = 'black';
+    }));
+});
